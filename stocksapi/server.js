1b5e3eac2896354993e0c79fd60bbaa91c370de9
Lijo John Daniel <lijojohndaniel@gmail.com>
2021-08-09 20:46:04 +0530

    Heroku

--- stocksapi/server.js
+++ stocksapi/server.js
@@ -1,16 +1,23 @@
 require('dotenv').config();
-const app = require('express')();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const login = require('./routes/loginRoutes/login');
 const stocks = require('./routes/stocksRoutes/stocks');
 const cors = require('cors');
 
+const PORT = process.env.PORT || 6060;
+
 app.use(bodyParser.json());
 app.use(cors());
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('./stocksreact/build'));
+}
 
 app.use('/login', login);
 app.use('/stocks', stocks);
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
--- stocksreact/src/index.js
+++ stocksreact/src/index.js
@@ -14,7 +14,7 @@ import { ToastContainer } from "react-toastify";
 
 const store = createStore(AppReducer, applyMiddleware(thunk));
 
-Axios.defaults.baseURL = "http://localhost:6060";
+Axios.defaults.baseURL = "https://stark-reaches-31057.herokuapp.com/";
 
 Axios.interceptors.request.use((config) => {
   const token = localStorage.getItem("stocks_token");
