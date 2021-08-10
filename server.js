a410f9c8032b6a9582def781857ed9b8dafad1b9
Lijo John Daniel <lijojohndaniel@gmail.com>
2021-08-11 02:20:53 +0530

    Heroku commit 8

--- server.js
+++ server.js
@@ -6,15 +6,12 @@ const login = require('./routes/loginRoutes/login');
 const stocks = require('./routes/stocksRoutes/stocks');
 const cors = require('cors');
 
-let HOST = 'localhost';
-
 app.use(bodyParser.json());
 app.use(cors());
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('./stocksreact/build'));
-    HOST = '0.0.0.0';
-}
+app.use(express.static('./stocksreact/build'));
+
+const HOST = '0.0.0.0';
 
 app.use('/login', login);
 app.use('/stocks', stocks);
