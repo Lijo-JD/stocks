93f4dc0e6493ea1ad06e4e8cd38434b5d5f8a837
Lijo John Daniel <lijojohndaniel@gmail.com>
2021-08-12 01:13:09 +0530

    Heroku commit 9

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const login = require('./routes/loginRoutes/login');
-const stocks = require('./routes/stocksRoutes/stocks');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const login = require("./routes/loginRoutes/login");
+const stocks = require("./routes/stocksRoutes/stocks");
+const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static('./stocksreact/build'));
+app.use(express.static("./stocksreact/build"));
 
-const HOST = '0.0.0.0';
+app.use("/login", login);
+app.use("/stocks", stocks);
 
-app.use('/login', login);
-app.use('/stocks', stocks);
-
-app.listen(process.env.PORT || 6060, HOST, () => console.log("Server running"));
+app.listen(process.env.PORT || 6060, () =>
+  console.log(`Server running on port ${process.env.PORT || 6060}`)
+);
 
 module.exports = app;
